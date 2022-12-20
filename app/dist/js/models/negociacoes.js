ace75ece4750a2c7107199427dad59ba384f0ae6
Joao Reis <joaooreis@hotmail.com>
2022-12-20 14:10:34 -0300

    first commit

---
+++ app/dist/js/models/negociacoes.js
@@ -0,0 +1,14 @@
+export class Negociacoes {
+    constructor() {
+        this.negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return this.negociacoes;
+    }
+    paraTexto() {
+        return JSON.stringify(this.negociacoes, null, 2);
+    }
+}
